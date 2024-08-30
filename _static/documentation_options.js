d535f4ee4d923b7a2d18952c7abc9da7a38cf3b4
syedhamidali <syedhamidali@users.noreply.github.com>
2024-08-30 04:29:44 +0000

    deploy: ebcf03141b438c74882a8b0eee6e4fd7ac600884

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '1.11.5.post449',
+    VERSION: '1.11.5.post472',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
